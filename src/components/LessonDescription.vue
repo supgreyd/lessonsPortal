0bfd687ec9e45a86ec48ca99219c298bf4af35db
Konstantin <supgreyd@gmail.com>
2023-04-25 19:02:41 +0300

    create ui components

---
+++ src/components/LessonDescription.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export default {
+    name: "LessonDescription",
+  };
+</script>
+
+<template>
+  <article>
+    <h2 class="text-xl font-bold mb-2">Lesson title</h2>
+    <p class="mt-2">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus posuere lobortis metus. Morbi
+      vehicula sem sed consectetur gravida. Etiam tempor rutrum ipsum eget convallis. Proin quis
+      suscipit erat, commodo cursus tellus. Cras commodo arcu vitae dolor fringilla commodo. Sed
+      aliquet elit luctus, gravida turpis sit amet, laoreet neque. Duis elit nulla, semper at quam
+      nec, porttitor placerat dolor. Maecenas vel erat diam. Nam vitae velit ut magna pulvinar
+      tempor. Nulla laoreet nisl risus, ut pretium sapien facilisis tempor.
+    </p>
+  </article>
+</template>
---
+++ src/components/LessonsListItem.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export default {
+    name: "LessonsListItem",
+  };
+</script>
+
+<template>
+  <div class="flex h-24">
+    <div class="w-1/4 bg-gray-600 flex justify-center items-center">
+      <svg
+        class="stroke-white"
+        height="24"
+        width="24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+      >
+        <path
+          stroke-linecap="round"
+          d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+        ></path>
+      </svg>
+    </div>
+    <div class="w-3/4 bg-orange-500 flex justify-center items-center">
+      <span class="text-2xl text-white">Lesson title</span>
+    </div>
+  </div>
+</template>
